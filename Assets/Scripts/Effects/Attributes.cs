ec536a52ee4f7014ad704b484bf66c64d435a7d9
Logan <giantpandagod@gmail.com>
2025-02-16 21:12:01 -0500

    Added enchantment backbone, deleted tutorial files

---
+++ Assets/Scripts/Effects/Attributes.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ATTRIBUTE {
+    flamable, red
+}
+
+public class Attributes : MonoBehaviour {
+    
+    public List<ATTRIBUTE> attributes;
+
+}
