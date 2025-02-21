1e340b4219d6cb55f844526ae2e78e2c10995812
Logiant <giantpandagod@gmail.com>
2025-02-21 16:43:13 -0500

    many levels become one level

--- Assets/Scripts/Player/ProgressionManager.cs
+++ Assets/Scripts/Player/ProgressionManager.cs
@@ -13,7 +13,16 @@ public class ProgressionManager : MonoBehaviour {
     public static ProgressionManager instance;
 
     public Vitality player;
-    public Transform spawnPoint;
+    [Header("List of Spawn Points for Progression")]
+    public Transform bossSpawn;
+    public Transform centralSpawn;
+    public Transform loganSpawn;
+    public Transform adiSpawn;
+
+    bool currentlyDying = false;
+
+    private int current_room = 0;
+
 
     public event System.EventHandler<Announcement> OnAnnouncement;
     public event System.EventHandler EndAnnouncement;
@@ -32,6 +41,7 @@ public class ProgressionManager : MonoBehaviour {
     public Weapon basicWeapon;
     public GameObject redBarrel;
     public Announcement secondDeath;
+    public Announcement SecondDeathP2;
 
 
 
@@ -54,29 +64,50 @@ public class ProgressionManager : MonoBehaviour {
             EndAnnouncement?.Invoke(this, System.EventArgs.Empty);
             announcing = false;
             if (checkpoint == 2) {
-                //load Level 2
-                UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+                Announce(SecondDeathP2);
+                checkpoint = 3;
+                announcing = true;
             }
         }   
     }
 
 
     void OnPlayerDeath(System.Object source, System.EventArgs e) {
-        float dist = (player.transform.position - spawnPoint.position).magnitude;
+        if (currentlyDying) return;
         numDeaths++;
 
         if (numDeaths == 1) {
+            float dist = (player.transform.position - bossSpawn.position).magnitude;
             Announce(firstDeath);
             UnlockTracker.instance.UnlockWeapon(basicWeapon);
-            StartCoroutine(MoveToSpawn(dist / timeScale));
+            StartCoroutine(MoveToSpawn(dist / timeScale, bossSpawn));
             checkpoint = 1;
         }
-        if (checkpoint == 1 && redBarrel == null) {
+        else if (checkpoint == 1 && redBarrel == null) {
             //play a death animation
             Announce(secondDeath);
             //load the next scene
             checkpoint = 2;
-            player.gameObject.SetActive(false);
+        }
+        else {
+            //generic revive based on where we were last
+            Transform destination = centralSpawn;
+            if (current_room == 0) {
+                //central spawn
+            }
+            if (current_room == 1) {
+                destination = loganSpawn;
+            }
+            if (current_room == 2) {
+                destination = adiSpawn;
+            }
+            if (current_room == 3) {
+                destination = bossSpawn;
+            }
+
+            float dist = (player.transform.position - destination.position).magnitude;
+            StartCoroutine(MoveToSpawn(dist / timeScale, destination));
+
         }
     }
 
@@ -92,18 +123,23 @@ public class ProgressionManager : MonoBehaviour {
         player.enabled = enabled;
     }
 
-    IEnumerator MoveToSpawn(float dt) {
+    IEnumerator MoveToSpawn(float dt, Transform spawn) {
         SetPlayerStatus(false);
+        currentlyDying = true;
         Vector3 p0 = player.transform.position;
+        Quaternion q0 = player.transform.rotation;
         float t0 = Time.time;
         while (Time.time - t0 < dt) {
-            player.transform.position = Vector3.Lerp(p0, spawnPoint.position, (Time.time - t0) / dt);
+            player.transform.position = Vector3.Lerp(p0, spawn.position, (Time.time - t0) / dt);
+            player.transform.rotation = Quaternion.Lerp(q0, spawn.rotation, (Time.time - t0) / dt);
+
             yield return null;
         }
-        player.transform.position = spawnPoint.position;
-        player.transform.rotation = spawnPoint.rotation;
+        player.transform.position = spawn.position;
+        player.transform.rotation = spawn.rotation;
         SetPlayerStatus(true);
         player.Reset();
+        currentlyDying = false;
         yield return null;
     }
 
