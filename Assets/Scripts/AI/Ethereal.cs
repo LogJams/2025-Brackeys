b0374192a4854302a31b5280a9ed06a85619194a
Logiant <giantpandagod@gmail.com>
2025-02-22 23:59:00 -0500

    Added some audio and a fixed ghosty boy
    also secret zoom!

--- Assets/Scripts/AI/Ethereal.cs
+++ Assets/Scripts/AI/Ethereal.cs
@@ -7,10 +7,16 @@ public class Ethereal : MonoBehaviour {
 
     public float delay = 0.5f;
 
+    public float min_alpha = 0.05f;
+
+    Material mat;
+
     Coroutine fade;
 
     void Awake() {
         owner = GetComponentInParent<Vitality>();
+        mat = owner.GetComponent<MeshRenderer>().material;
+
     }
 
     void OnTriggerEnter(Collider other) {
@@ -23,10 +29,24 @@ public class Ethereal : MonoBehaviour {
         if (other.CompareTag("Player")) {
             owner.RemoveStatusEffect(EFFECTS.ethereal);
         }
+        Color c = mat.color;
+        c.a = 1;
+        mat.color = c;
     }
 
     IEnumerator FadeAway() {
-        yield return new WaitForSeconds(delay);
+        float t0 = Time.time;
+
+        Color c = mat.color;
+
+        while (Time.time - t0 <= delay) {
+            c.a = Mathf.Lerp(1, min_alpha, (Time.time - t0) / delay);
+            mat.color = c;
+            yield return null;
+        }
+
+        c.a = min_alpha;
+        
         owner.AddStatusEffect(EFFECTS.ethereal, -1);
         yield return null;
     }
